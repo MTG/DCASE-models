c5b023d0c5ca6b970c3ed56dc8499bb1a8d92d2b
Pablo Zinemanas <pzinemanas@fing.edu.uy>
2020-06-16 13:14:25 +0200

    update test scripts

--- tests/test_fine_tuning.py
+++ tests/test_fine_tuning.py
@@ -6,12 +6,11 @@ import argparse
 
 sys.path.append('../')
 from dcase_models.utils.files import load_json, mkdir_if_not_exists
-from dcase_models.data.data_generator import *
-from dcase_models.model.container import *
-from dcase_models.model.models import *
-from dcase_models.data.feature_extractor import *
+from dcase_models.data.data_generator import DataGenerator
+from dcase_models.model.models import get_available_models
+from dcase_models.data.features import get_available_features
+from dcase_models.data.datasets import get_available_datasets
 from dcase_models.data.scaler import Scaler
-from dcase_models.utils.misc import get_class_by_name
 
 os.environ["CUDA_VISIBLE_DEVICES"] = "1"
 
@@ -29,7 +28,7 @@ params_dataset = params["datasets"][args.destination_dataset]
 params_features = params["features"]
 
 # get feature extractor class
-feature_extractor_class = get_class_by_name(globals(), args.features, FeatureExtractor)
+feature_extractor_class = get_available_features()[args.features]
 # init feature extractor
 feature_extractor = feature_extractor_class(sequence_time=params_features['sequence_time'], 
                                             sequence_hop_time=params_features['sequence_hop_time'], 
@@ -39,28 +38,21 @@ feature_extractor = feature_extractor_class(sequence_time=params_features['seque
                                             sr=params_features['sr'], **params_features[args.features])
 
 # get destination dataset class
-data_generator_class = get_class_by_name(globals(), args.destination_dataset, DataGenerator)
+dataset_class = get_available_datasets()[args.destination_dataset]
+dataset = dataset_class(params_dataset['dataset_path'])
 
 kwargs = {}
 if args.destination_dataset == 'URBAN_SED':
     kwargs = {'sequence_hop_time': params['features']['sequence_hop_time']}
-data_generator = data_generator_class(params_dataset['dataset_path'], params_dataset['feature_folder'], args.features, 
-                                      audio_folder=params_dataset['audio_folder'], **kwargs)
+data_generator = DataGenerator(dataset, feature_extractor, **kwargs)
 
-# extract features if needed
-folders_list = data_generator.get_folder_lists()
-for audio_features_paths in folders_list:
-    print('Extracting features from folder: ', audio_features_paths['audio'])
-    response = feature_extractor.extract(audio_features_paths['audio'], audio_features_paths['features'])
-    if response is None:
-        print('Features already were calculated, continue...')
-    print('Done!')
+data_generator.extract_features()
 
 # load data
 data_generator.load_data()
 
 # get model class
-model_container_class = get_class_by_name(globals(), args.model, DCASEModelContainer)
+model_container_class = get_available_models()[args.model]
 params_model = params["models"][args.model]
 
 # load data
@@ -72,16 +64,17 @@ X_train = scaler.transform(X_train)
 X_val = scaler.transform(X_val)
 
 # paths
-exp_folder_source = os.path.join(args.model, args.source_dataset)
+folder_models = 'models'
+exp_folder_source = os.path.join(folder_models, args.model, args.source_dataset)
 exp_folder_source_fold = os.path.join(exp_folder_source, fold_test)
 
-exp_folder_destination = os.path.join(args.model, args.source_dataset + '_ft_' + args.destination_dataset)
+exp_folder_destination = os.path.join(folder_models, args.model, args.source_dataset + '_ft_' + args.destination_dataset)
 mkdir_if_not_exists(exp_folder_destination)
 exp_folder_destination_fold = os.path.join(exp_folder_destination, fold_test)
 mkdir_if_not_exists(exp_folder_destination_fold)
 
 # load source model and weights
-model_container = model_container_class(model=None, folder=exp_folder_source_fold)
+model_container = model_container_class(model=None, model_path=exp_folder_source_fold)
 model_container.load_model_weights(exp_folder_source_fold)
 model_container.model.summary()
 
